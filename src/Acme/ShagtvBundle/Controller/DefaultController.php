f0c3987347f1533b0ee212dc696798f4596552e9
shagtv <shagtv@gmail.com>
2015-03-27 00:06:48 +0300

    fix

--- src/Acme/ShagtvBundle/Controller/DefaultController.php
+++ src/Acme/ShagtvBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Acme\ShagtvBundle\Controller;
 
 use Acme\ShagtvBundle\Entity\Comments;
 use Acme\ShagtvBundle\Entity\Posts;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,7 +15,8 @@ class DefaultController extends Controller {
 	public function indexAction() {
 		$posts = $this->getDoctrine()
 			->getRepository('AcmeShagtvBundle:Posts')
-			->findAll();
+			->findBy(array(), array(), 5, 0);
+
 		return $this->render('AcmeShagtvBundle:Default:index.html.twig', array('posts' => $posts));
 	}
 
@@ -41,6 +43,77 @@ class DefaultController extends Controller {
 			->getRepository('AcmeShagtvBundle:Posts')
 			->find((int)$id);
 		return $this->render('AcmeShagtvBundle:Default:info.html.twig', array('post' => $post));
+	}
+
+	public function editAction(Request $request, $id) {
+		$post = $this->getDoctrine()
+			->getRepository('AcmeShagtvBundle:Posts')
+			->find((int)$id);
+
+		$form = $this->createFormBuilder($post)
+			->add('title', 'text', array('label' => 'Заголовок'))
+			->add('text', 'textarea', array('label' => 'Текст'))
+			//->add('user', 'choice', array('label' => 'Пользователь'))
+			->add('user', 'entity', array(
+				'class' => 'Acme\ShagtvBundle\Entity\Users',
+				'property' => 'name'
+			))
+			->add('save', 'submit', array('label' => 'Сохранить'))
+			->getForm();
+
+
+		$form->handleRequest($request);
+
+		if ($form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($post);
+			$em->flush();
+
+			return $this->redirectToRoute('acme_shagtv_info', array('id' => $post->getId()));
+		}
+
+		return $this->render('AcmeShagtvBundle:Default:edit.html.twig', array('form' => $form->createView(), 'post' => $post));
+	}
+
+	public function newAction(Request $request) {
+		// create a task and give it some dummy data for this example
+		$post = new Posts();
+
+		$form = $this->createFormBuilder($post)
+			->add('title', 'text', array('label' => 'Заголовок'))
+			->add('text', 'textarea', array('label' => 'Текст'))
+			//->add('user', 'choice', array('label' => 'Пользователь'))
+			->add('user', 'entity', array(
+				'class' => 'Acme\ShagtvBundle\Entity\Users',
+				'property' => 'name'
+			))
+			->add('save', 'submit', array('label' => 'Создать'))
+			->getForm();
+
+
+		$form->handleRequest($request);
+
+		if ($form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($post);
+			$em->flush();
+
+			return $this->redirectToRoute('acme_shagtv_info', array('id' => $post->getId()));
+		}
+
+		return $this->render('AcmeShagtvBundle:Default:new.html.twig', array('form' => $form->createView(),));
+	}
+
+	public function removeAction($id) {
+		$post = $this->getDoctrine()
+			->getRepository('AcmeShagtvBundle:Posts')
+			->find((int)$id);
+
+		$em = $this->getDoctrine()->getManager();
+		$em->remove($post);
+		$em->flush();
+
+		return $this->redirectToRoute('acme_shagtv_index');
 	}
 
 	public function helloAction($name) {
