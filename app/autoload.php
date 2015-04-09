e398f11f104d2131f5bf7e3273027963413bb9fa
shagtv <shagtv@nodasoft.com>
2015-04-09 11:56:20 +0300

    add crud

--- app/autoload.php
+++ app/autoload.php
@@ -8,6 +8,8 @@ use Composer\Autoload\ClassLoader;
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
+$loader->add('Jns', __DIR__.'/../src');
+
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
